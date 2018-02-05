a9d65c9a4057dacf1c5ba9762266f4736b630c72
Marcin Zawada <zawadsoon@gmail.com>
2018-02-05 22:52:53 +0100

    Added StringArray and int, modified build method, now array of items can be passed.

--- src/magento-xml.js
+++ src/magento-xml.js
@@ -6,7 +6,6 @@
 const parts = {
 
     root: {
-        // language=XML
         start: '' +
         '<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
             'xmlns:xsd="http://www.w3.org/2001/XMLSchema" ' +
@@ -27,13 +26,25 @@ const parts = {
 
     variable: {
         string: (name, value) => {
+            if (typeof value === 'undefined' || value === null) return '';
             return '<' + name + ' xsi:type="xsd:string">' + value + '</' + name + '>';
+        },
+        stringArray: (name, array) => {
+            if (!(array instanceof Array)) return '';
+            let xml = '<' + name + ' SOAP-ENC:arrayType="xsd:string[' + array.length + ']" xsi:type="ns1:ArrayOfString">';
+            for (let item in array)
+                xml += '<item xsi:type="xsd:string">' + item + '</item>';
+            xml += '</' + name + '>';
+        },
+        int: (name, value) => {
+            if (typeof value === 'undefined' || value === null) return '';
+            return '<' + name + ' xsi:type="xsd:int">' + value + '</' + name + '>';
         }
     },
 
     method: (name, content) => {
-        // language=XML
-        return '<urn:' + name + ' soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">' + content + '</urn:' + name + '>';
+        return '<urn:' + name + ' soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">' +
+            content + '</urn:' + name + '>';
     },
 
 };
@@ -44,8 +55,14 @@ const parts = {
  * @param method {string} - name of called method in SOAP API
  * @return {string} + full XML request
  */
-function build (method, customXMLString) {
-    return parts.root.start + parts.header.start + parts.header.end + parts.body.start + parts.method(method, customXMLString) + parts.body.end + parts.root.end;
+function build (method, customXMLArray) {
+    let customXMLString = '';
+
+    for (let xmlPart in customXMLArray)
+        customXMLString += customXMLArray[xmlPart];
+
+    return parts.root.start + parts.header.start + parts.header.end + parts.body.start +
+        parts.method(method, customXMLString) + parts.body.end + parts.root.end;
 }
 
 module.exports = {
