dfff1037955e07b8fd1fc9bd0054eadae1a8cb64
Marcin <zawadsoon@gmail.com>
2018-02-14 23:33:04 +0100

    Changed files convention
    Added customer address example, added entity file wich will store entities. Changed files names.

---
+++ examples/customer_address.js
@@ -0,0 +1,14 @@
+const Magento19xAPI = require("./../src/magento19xapi");
+const config = require("./../config");
+
+let magento = new Magento19xAPI (config.apiUrl, config.headers);
+let customerId = 17678;
+
+magento.login(config.username, config.apiKey).then((sessionId) => {
+    console.log('SessionId:', sessionId);
+    return magento.customerAddressList({customerId: customerId});
+}).then((list) => {
+    console.log('Address list: ', list);
+}).catch((error) => {
+    console.error(error);
+});
